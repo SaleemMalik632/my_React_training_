62bb8fd75fdff70269634508cc20bae43ebd5d6a
SaleemMalik632 <saleemalik444@gmail.com>
2023-07-21 23:07:14 +0500

    some changes

--- text/src/App.js
+++ text/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Edit src App.js and save to reload.</p>
+         <h1> how to and make good ui </h1>
+        <a className="App-link"href="https://reactjs.org"  target="_blank"  rel="noopener noreferrer" > Learn React  </a>
+        <a href=  "https://reactjs.org" className="mail" target="_blank" rel="noopener noreferrer" > Learn how to sex  </a>
       </header>
-    </div>
+    </div> 
   );
 }
 
-export default App;
+// export default App;
