f2a0a04a638720d5747929ed4ca9900b2289315b
SaleemMalik632 <saleemalik444@gmail.com>
2023-07-23 10:40:58 +0500

    useState

--- text/src/App.js
+++ text/src/App.js
@@ -1,40 +1,41 @@
 import "./App.css";
 import { useState } from "react";
-import   Footer  from './Component/Footer'
-import   Header  from './Component/Header' ;
-import  Style  from './Component/StyleCompoment'
-import Props  from "./Component/Props";
-import EcommercePage  from  './Component/call';
-
+import Footer from "./Component/Footer";
+import Header from "./Component/Header";
+import Style from "./Component/StyleCompoment";
+import Props from "./Component/Props";
+import EcommercePage from "./Component/call";
 
 function MyButton() {
-  const [count, setCount] = useState(10);  
-  function fun(){ 
-    setCount(count + 1);
+  const [count, setCount] = useState(0); 
+  function fun() {
+    setCount(count + 1); 
   }
+  function dec() {
+    setCount(count - 1);
+  } 
   return (
-    <button  onClick={fun}> 
-      {count} time on it 
-    </button>
+    <div>
+      <p>{count}</p>
+      <button onClick={fun}> Add  </button>
+      <button onClick={dec}> Decrease  </button>  
+    </div>
   );
-} 
-
-
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h5>Below Me All the Components </h5>
-          <Header/> 
-          <Footer/>
-          <MyButton/>  
-        <Style /> 
+        <Header />
+        <Footer />
+        <MyButton />
+        <Style />
         <EcommercePage />
-        <Props name = "saleem" age = "nnnlik"  number = {0}/>    
-      </header> 
-
-    </div>     
+        <Props name="saleem" age="nnnlik" number={0} />
+      </header>
+    </div>
   );
 }
 
